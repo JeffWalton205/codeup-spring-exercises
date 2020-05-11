5f0edac8612bcfc2fdcf9bd014d536e127408268
Jeff Walton <jeff.walton205@gmail.com>
2020-05-11 11:58:33 -0500

    first commit

---
+++ src/main/java/com/codeup/demo/FortunaSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunaSpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunaSpringBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/demo/FortunaSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunaSpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
